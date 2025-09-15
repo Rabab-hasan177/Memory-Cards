2e8d2923ea7eb57c47ffedbca3ab7d9023b461dd
unknown <rabab177hasan@gmail.com>
2025-09-15 09:57:55 +0300

    images added in js

--- script.js
+++ script.js
@@ -1,15 +1,26 @@
-const cards = document.querySelectorAll(".memory-card")
+const myContainor = document.querySelector(".container")
+const fruitImages = [
+  { src: "./pictures/apple.jpg", alt: "apple" },
+  { src: "./pictures/banana.jpg", alt: "banana" },
+  { src: "./pictures/orange.jpg", alt: "orange" },
+  { src: "./pictures/pear1.jpg", alt: "pear1" },
+  { src: "./pictures/strawberry.jpg", alt: "strawberry" },
+  { src: "./pictures/apple.jpg", alt: "apple" },
+  { src: "./pictures/banana.jpg", alt: "banana" },
+  { src: "./pictures/orange.jpg", alt: "orange" },
+  { src: "./pictures/pear1.jpg", alt: "pear1" },
+  { src: "./pictures/strawberry.jpg", alt: "strawberry" },
+]
+const backImage = [{src: "./pictures/Qmark.png", alt:"qmark"}]
 
-let cardFlipped = false
-let firstCard;
-let secondCard;
+for (let i = 0; i < fruitImages.length; i++) {
+  let card = document.createElement("div")
+  let img1 = document.createElement("img")
+  img.classList.add('front')
 
-const flippingCard = () => {
-  console.log("click")
-
-
-card.classList.toggle("flipped")`
-
-cards.forEach((card) => {
-  card.addEventListener("click", flippingCard)
-})
+  img.setAttribute("src", fruitImages[i].src)
+  card.appendChild(img)
+  let variable = fruitImages[i]
+  myContainor.appendChild(card)
+  console.log(variable)
+}
