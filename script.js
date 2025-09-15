dc848c24d70d44d79c5b35655f42b419c556ed1d
unknown <rabab177hasan@gmail.com>
2025-09-15 10:31:21 +0300

    front and back images added and css done

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const myContainor = document.querySelector(".container")
+const myContainor = document.querySelector(".play-game")
 const fruitImages = [
   { src: "./pictures/apple.jpg", alt: "apple" },
   { src: "./pictures/banana.jpg", alt: "banana" },
@@ -11,16 +11,27 @@ const fruitImages = [
   { src: "./pictures/pear1.jpg", alt: "pear1" },
   { src: "./pictures/strawberry.jpg", alt: "strawberry" },
 ]
-const backImage = [{src: "./pictures/Qmark.png", alt:"qmark"}]
+const backImage = [{ src: "./pictures/Qmark.png", alt: "qmark" }]
 
 for (let i = 0; i < fruitImages.length; i++) {
   let card = document.createElement("div")
-  let img1 = document.createElement("img")
-  img.classList.add('front')
+card.classList.add("memory-card")
+  let frontImg = document.createElement("img")
+  frontImg.classList.add("front")
+  frontImg.setAttribute("src", fruitImages[i].src)
+  card.appendChild(frontImg)
+  for (let j = 0; j < backImage.length; j++) {
+    let backImg = document.createElement("img")
+    backImg.classList.add("back")
+    backImg.setAttribute("src", backImage[j].src)
+    card.appendChild(backImg)
+    let variable2 = backImage[j]
+  }
 
-  img.setAttribute("src", fruitImages[i].src)
-  card.appendChild(img)
   let variable = fruitImages[i]
+
   myContainor.appendChild(card)
+
   console.log(variable)
 }
+
